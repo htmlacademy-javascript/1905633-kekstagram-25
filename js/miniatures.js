61499372d06ecfd87832e726b743f7a01d41d5fb
Rybus <artem.amerov@gmail.com>
2022-04-01 13:40:14 +0300

    Добавляет миниатюры (Первая домашка 7-ого модуля)

---
+++ js/miniatures.js
@@ -0,0 +1,20 @@
+const miniaturesDisplay = function (objects) {
+  const picturesSection = document.querySelector('.pictures');
+  const templateContent = document.querySelector('#picture').content;
+  const pictureContainer = templateContent.querySelector('.picture');
+
+  for (let i = 0; i < objects.length; i++) {
+    const pictureContainerCloned = pictureContainer.cloneNode(true);
+    const picture = pictureContainerCloned.querySelector('.picture__img');
+    const pictureLikes = pictureContainerCloned.querySelector('.picture__likes');
+    const pictureComments = pictureContainerCloned.querySelector('.picture__comments');
+
+    picture.src = objects[i].url;
+    pictureLikes.textContent = objects[i].likes;
+    pictureComments.textContent = objects[i].comments.length - 1;
+
+    picturesSection.append(pictureContainerCloned);
+  }
+};
+
+export { miniaturesDisplay };
